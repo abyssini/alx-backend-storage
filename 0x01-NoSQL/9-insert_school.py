10b4857513badfb2f63520b505a6ea3e199b4034
abyssini <abyssiniazelalem874@gmail.com>
2023-02-16 21:19:01 +0300

    nosql

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''python function to insert documents in a collection
+'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    ''' insert document in a collection
+    '''
+    result = mongo_collection.insert_one(kwargs).inserted_id
+    return result
